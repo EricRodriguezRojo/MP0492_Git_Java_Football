8f58b3ae803310541f96283b3d06f18b2fe660b4
Abril Palau Pardillos <abrilpalau14@gmail.com>
2025-09-18 16:05:54 +0200

    The class player created

---
+++ src/worldcup/Player.java
@@ -0,0 +1,56 @@
+package worldcup;
+
+/**
+ * Class to define the midfielder
+ */
+
+/*All the class Player generated*/
+
+public class Player{
+	String name;
+	String nickname;
+	int age;
+	String position;
+	
+	public Player(String name, String nickname, int age, String position) {
+		super();
+		this.name = name;
+		this.nickname = nickname;
+		this.age = age;
+		this.position = position;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+	
+}
